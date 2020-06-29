4e002c0be180d38d1e9da05a11b74938dce4e057
Aaron Abramov <dabramov@fb.com>
2020-06-28 21:13:50 -0500

    🦀

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,33 @@
+mod drains;
 
+use crate::drains::Drain;
+use std::time::SystemTime;
+
+use anyhow::Result;
+pub struct Logger {
+    drains: Vec<Box<dyn Drain>>,
+}
+
+impl Logger {
+    pub fn with_event<E: Into<String>, F, T>(event_name: E, f: F) -> Result<T>
+    where
+        F: FnOnce(Event) -> Result<T>,
+    {
+        let e = Event::new(event_name.into());
+        f(e)
+    }
+}
+
+pub struct Event {
+    name: String,
+    timestamp: SystemTime,
+}
+
+impl Event {
+    fn new(event_name: String) -> Self {
+        Self {
+            name: event_name,
+            timestamp: SystemTime::now(),
+        }
+    }
+}
--- tests/mod.rs
+++ tests/mod.rs
@@ -1,4 +1,5 @@
 use k9::*;
+use ll;
 
 #[test]
 fn test() {

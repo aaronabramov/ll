3272cd1d4f5fdf54dcad015d9c5790e1a5d6182c
Aaron Abramov <aaron@abramov.io>
2021-06-21 23:08:41 -0500

    tree formatting

--- ll3/src/lib.rs
+++ ll3/src/lib.rs
@@ -5,8 +5,4 @@ mod uniq_id;
 
 pub use task::Task;
 
-pub mod reporters {
-    mod term_status;
-
-    pub use term_status::TermStatus;
-}
+pub mod reporters;
---
+++ ll3/src/reporters/mod.rs
@@ -0,0 +1,8 @@
+mod term_status;
+mod text;
+
+pub use term_status::TermStatus;
+
+pub trait Reporter {
+    fn task_start(task: &crate::task_internal::TaskInternal);
+}

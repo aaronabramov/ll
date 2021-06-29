22634c545d4618775e168870893d2c01ec4b4cbe
Aaron Abramov <aaron@abramov.io>
2021-06-28 22:12:13 -0500

    3.0.5

--- ll/src/lib.rs
+++ ll/src/lib.rs
@@ -15,9 +15,11 @@ pub use task_tree::add_reporter;
 #[cfg(test)]
 mod tests;
 
+pub use data::{Data, DataEntry, DataValue};
 pub use reporters::term_status::TermStatus;
 pub use reporters::text::StdoutReporter;
 pub use reporters::text::StringReporter;
+pub use task_tree::TaskInternal;
 pub use task_tree::TaskTree;
 
 pub use reporters::term_status::stdio::stdout;
